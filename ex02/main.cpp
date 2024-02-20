e1a84c3b27201ea982f2bcaf4e04f5543f148476
phudyka <paul.hudyka@gmail.com>
2024-02-20 15:06:53 +0100

    ex02 good version2

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 16:53:19 by phudyka           #+#    #+#             */
-/*   Updated: 2024/02/20 15:05:36 by phudyka          ###   ########.fr       */
+/*   Updated: 2024/02/20 15:06:44 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     if (argc <= 1)
     {
